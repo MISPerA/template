89dd4fb38420616d2971635d2bc64b0fe6ef3905
MISPerA <143564926+MISPerA@users.noreply.github.com>
2023-11-16 08:53:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -28,12 +28,12 @@ for (var i = 0; i < versiculosArray.length; i++) {
         templateHTML += '<div class="verse-container">';
         templateHTML += '<p class="verse-title">' + titulo + '</p>';
         templateHTML += '<p class="verse-text">' + texto + '</p>';
-        templateHTML += '<div class="instagram-link" href="https://www.instagram.com/mis.pera/" target="_blank">';
+        templateHTML += '<a class="instagram-link" href="https://www.instagram.com/mis.pera/" target="_blank">';
         templateHTML += '<i class="fab fa-instagram" style="color: black;"></i>'; // Ícone do Instagram
-        //templateHTML += '<div class=class="instagram" href="https://www.instagram.com/mis.pera/" target="_blank">@mis.pera
-        //templateHTML += '<a href="https://www.instagram.com/mis.pera/" target="_blank" style="color: #01BAD2; text-decoration: none; margin-left: 5px;">@mis.pera</a>';
-        templateHTML += '</div>';
-        templateHTML += '</div>';
+        //templateHTML += '<div class="instagram" href="https://www.instagram.com/mis.pera/" target="_blank">@mis.pera
+        templateHTML += '<a href="https://www.instagram.com/mis.pera/" target="_blank" style="color: #01BAD2; text-decoration: none; margin-left: 5px;">@mis.pera</a>';
+        templateHTML += '</a>';
+        templateHTML += '</a>';
     }
 }
 
