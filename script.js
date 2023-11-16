69f12d04d6865ddfd351b731dc7dc94c02cbcec8
MISPerA <143564926+MISPerA@users.noreply.github.com>
2023-11-15 23:38:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
-function gerarTemplate() {
-    var versiculo = document.getElementById('versiculo').value;
+function gerarTemplates() {
+    var versiculos = document.getElementById('versiculos').value;
     var outputDiv = document.getElementById('output');
 
-    // Verifica se o campo do versículo não está vazio
-    if (versiculo.trim() === '') {
-        alert('Por favor, insira um versículo bíblico.');
+    // Verifica se o campo dos versículos não está vazio
+    if (versiculos.trim() === '') {
+        alert('Por favor, insira pelo menos um versículo bíblico.');
         return;
     }
 
-    // Gera o HTML para o template
-    var templateHTML = '<div style="border: 1px solid #000; padding: 10px; margin-bottom: 10px;">';
-    templateHTML += '<p style="font-size: 16px; font-weight: bold;">Versículo Bíblico:</p>';
-    templateHTML += '<p style="font-size: 14px;">' + versiculo + '</p>';
-    templateHTML += '</div>';
+    // Separa os versículos com base nas quebras de linha
+    var versiculosArray = versiculos.split('\n');
+    var templateHTML = '';
 
-    // Adiciona o template ao elemento de saída
+    // Gera o HTML para cada versículo
+    for (var i = 0; i < versiculosArray.length; i++) {
+        var versiculo = versiculosArray[i].trim();
+        if (versiculo !== '') {
+            templateHTML += '<div style="border: 1px solid #000; padding: 10px; margin-bottom: 10px;">';
+            templateHTML += '<p style="font-size: 16px; font-weight: bold;">Versículo Bíblico:</p>';
+            templateHTML += '<p style="font-size: 14px;">' + versiculo + '</p>';
+            templateHTML += '</div>';
+        }
+    }
+
+    // Adiciona os templates ao elemento de saída
     outputDiv.innerHTML = templateHTML;
 }
 
