05bf7df1d8faa545c42cc7956b92d44327c070f0
MISPerA <143564926+MISPerA@users.noreply.github.com>
2023-11-15 23:46:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,29 +8,21 @@ function gerarTemplates() {
         return;
     }
 
-    // Separa os versículos com base nas quebras de linha
-    var versiculosArray = versiculos.split('\n');
+    // Separa os versículos com base em duas quebras de linha
+    var versiculosArray = versiculos.split('\n\n');
     var templateHTML = '';
 
     // Gera o HTML para cada versículo
     for (var i = 0; i < versiculosArray.length; i++) {
         var versiculo = versiculosArray[i].trim();
         if (versiculo !== '') {
-            templateHTML += '<div style="border: 1px solid #000; padding: 10px; margin-bottom: 10px;">';
-            templateHTML += '<p style="font-size: 16px; font-weight: bold;">Versículo Bíblico:</p>';
-            templateHTML += '<p style="font-size: 14px;">' + versiculo + '</p>';
+            templateHTML += '<div class="verse-container">';
+            templateHTML += '<p class="verse-title">Versículo Bíblico:</p>';
+            templateHTML += '<p class="verse-text">' + versiculo + '</p>';
             templateHTML += '</div>';
         }
     }
 
     // Adiciona os templates ao elemento de saída
     outputDiv.innerHTML = templateHTML;
-}
-
-function imprimir() {
-    var conteudo = document.getElementById('output').innerHTML;
-    var janelaImpressao = window.open('', '_blank');
-    janelaImpressao.document.write('<html><head><title>Imprimir</title></head><body>' + conteudo + '</body></html>');
-    janelaImpressao.document.close();
-    janelaImpressao.print();
 }
